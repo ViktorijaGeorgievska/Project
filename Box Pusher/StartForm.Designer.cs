994e0b595f07d22eeb3d13911b318836928b0c58
dameey <i.damjan.57@gmail.com>
2025-08-27 20:43:06 +0200

    Add files via upload

---
+++ Box Pusher/StartForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Project
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnInstructions = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(82, 67);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(174, 26);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "Играј";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnInstructions
+            // 
+            this.btnInstructions.Location = new System.Drawing.Point(82, 119);
+            this.btnInstructions.Name = "btnInstructions";
+            this.btnInstructions.Size = new System.Drawing.Size(174, 24);
+            this.btnInstructions.TabIndex = 1;
+            this.btnInstructions.Text = "Инструкции";
+            this.btnInstructions.UseVisualStyleBackColor = true;
+            this.btnInstructions.Click += new System.EventHandler(this.btnInstructions_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(82, 167);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(174, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Излези";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(119, 34);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(94, 16);
+            this.lblName.TabIndex = 3;
+            this.lblName.Text = "BOX PUSHER";
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 220);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnInstructions);
+            this.Controls.Add(this.btnPlay);
+            this.Name = "StartForm";
+            this.Text = "Мени";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnInstructions;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblName;
+    }
+}
