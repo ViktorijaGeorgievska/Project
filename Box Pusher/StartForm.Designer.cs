6cf59f7c92cba1a42caf7a0d236667a906fab31b
Viktorija <viktorijageorgievska4@gmail.com>
2025-08-28 19:02:16 +0200

    Update StartForm.Designer.cs

--- Box Pusher/StartForm.Designer.cs
+++ Box Pusher/StartForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Project
+namespace Project
 {
     partial class StartForm
     {
@@ -36,40 +36,48 @@
             // 
             // btnPlay
             // 
-            this.btnPlay.Location = new System.Drawing.Point(82, 67);
+            this.btnPlay.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.Location = new System.Drawing.Point(82, 83);
             this.btnPlay.Name = "btnPlay";
-            this.btnPlay.Size = new System.Drawing.Size(174, 26);
+            this.btnPlay.Size = new System.Drawing.Size(174, 34);
             this.btnPlay.TabIndex = 0;
             this.btnPlay.Text = "Играј";
-            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.UseVisualStyleBackColor = false;
             this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
             // 
             // btnInstructions
             // 
-            this.btnInstructions.Location = new System.Drawing.Point(82, 119);
+            this.btnInstructions.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInstructions.Location = new System.Drawing.Point(82, 123);
             this.btnInstructions.Name = "btnInstructions";
-            this.btnInstructions.Size = new System.Drawing.Size(174, 24);
+            this.btnInstructions.Size = new System.Drawing.Size(174, 34);
             this.btnInstructions.TabIndex = 1;
             this.btnInstructions.Text = "Инструкции";
-            this.btnInstructions.UseVisualStyleBackColor = true;
+            this.btnInstructions.UseVisualStyleBackColor = false;
             this.btnInstructions.Click += new System.EventHandler(this.btnInstructions_Click);
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(82, 167);
+            this.btnCancel.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(82, 163);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(174, 23);
+            this.btnCancel.Size = new System.Drawing.Size(174, 34);
             this.btnCancel.TabIndex = 2;
             this.btnCancel.Text = "Излези";
-            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.UseVisualStyleBackColor = false;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // lblName
             // 
             this.lblName.AutoSize = true;
-            this.lblName.Location = new System.Drawing.Point(119, 34);
+            this.lblName.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(116, 44);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(94, 16);
+            this.lblName.Size = new System.Drawing.Size(130, 20);
             this.lblName.TabIndex = 3;
             this.lblName.Text = "BOX PUSHER";
             // 
